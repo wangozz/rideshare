ec84854a311bf113c0441ee132d256ea74b16f77
SamuelWango <wangos478@gmail.com>
2023-12-31 03:46:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// this file basically collects all the pages and make them work as a unit
+import React from 'react';
+import LandingPage from './landingpage';
+
+const App = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/landingpage.js
@@ -0,0 +1,12 @@
+// This is the first page of the app
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Rideshare App</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
